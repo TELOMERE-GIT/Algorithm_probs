e221147dd1999d53bc58ccb1714c67e296351734
TELOMERE-GIT <76815611+TELOMERE-GIT@users.noreply.github.com>
2024-11-02 16:27:40 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 388 ms, Memory: 5924 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+#include<cstdio>
+using namespace std;
+
+
+int main(){
+	int N;
+	cin>>N;
+	int *arr = new int [N];
+	for(int i=0; i<N; ++i){
+		 scanf("%d",&arr[i]);
+	}
+	sort(arr,arr+N);
+	for(int i=0; i<N; ++i){
+		printf("%d\n",arr[i]);
+	}
+	delete arr;
+}
