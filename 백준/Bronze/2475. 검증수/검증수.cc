84edda9e719c28b7ad9e242fe83e35396c9504f5
TELOMERE-GIT <76815611+TELOMERE-GIT@users.noreply.github.com>
2024-11-04 09:36:28 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int arr[5];
+	for(int i=0; i<5; i++){
+		cin>>arr[i];
+		arr[i]=arr[i]*arr[i];
+	}
+	int sum=0;
+	for(int i=0; i<5; i++){
+		sum+=arr[i];
+	}
+	cout<<sum%10;
+	
+}
