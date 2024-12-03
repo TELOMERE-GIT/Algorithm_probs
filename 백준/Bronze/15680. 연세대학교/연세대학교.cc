c5b2efa49e0663250020b15a950537ef0a72457a
TELOMERE-GIT <76815611+TELOMERE-GIT@users.noreply.github.com>
2024-12-03 18:17:51 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	int a;
+	string ans;
+	cin>>a;
+	if(a==0) cout<<"YONSEI";
+	else cout<<"Leading the Way to the Future";
+}
